3faf0c2de84303eb18917b70fe96049e25beb87c
foziahussaini <fauziakhudadad@gmail.com>
2025-12-28 00:48:17 +0430

    customise

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:assets_audio_player/assets_audio_player.dart';
-
 void main() {
   runApp(const XylophoneApp());
 }
@@ -17,10 +17,34 @@ class _XylophoneAppState extends State<XylophoneApp> {
   double width = 310;
   double height = 60;
   EdgeInsets padding = EdgeInsets.symmetric(horizontal: 42, vertical: 2,);
-  void onLetterTap(String letter){
+  List<String> letters = ["A", "B", "C", "D", "E", "F", "G"];
+  String randomLetter = "A";
+
+  void generateRandomLetter(){
     setState(() {
-      AssetsAudioPlayer.newPlayer().open(Audio("assets/audio/$letter.wav"),);
+      randomLetter = letters[Random().nextInt(letters.length)];
     });
+  }
+
+  void onLetterTap(String letter){
+    if(letter == randomLetter){
+      AssetsAudioPlayer.newPlayer().open(
+        Audio("assets/audio/$letter.wav"),);
+    generateRandomLetter();}
+  }
+
+  GestureDetector buildNoteContainer({required String letter, required Color color, required double width,}){
+    return GestureDetector(
+      onTap: (){
+        onLetterTap(letter);},
+      child: Container(
+        width: width,
+        height: height,
+        padding: padding,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12),
+          color: color,),
+        child: Center(child: Text(letter, style: TextStyle(fontSize: 30, color: Colors.white),)),),
+    );
   }
 
   @override
@@ -32,111 +56,35 @@ class _XylophoneAppState extends State<XylophoneApp> {
         body: Center(
           child: SizedBox(
             width: 300,
-            height: 540,
+            height: 780,
             child: Card(
               elevation: 24,
               color: Colors.brown.withOpacity(0.02),
               shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12),),
                 child: Column(mainAxisAlignment: MainAxisAlignment.center,
                   children: [
+                    Text(randomLetter, style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.white),),
+                    SizedBox(height: 40,),
                     // first row
-                        GestureDetector(
-                          onTap: (){
-                            onLetterTap("A");},
-                          child: Container(
-                            width: width,
-                            height: height,
-                            padding: padding,
-                            decoration: BoxDecoration(borderRadius: BorderRadius.circular(12),
-                            color: Colors.red[900],),
-                            child: Text("A 🎶", style: TextStyle(fontSize: 30, color: Colors.white),),),
-                        ),
+                    buildNoteContainer(letter: "A", color: Colors.red, width: width),
                     // second row
                     SizedBox(height: sizedBox,),
-                        GestureDetector(
-                          onTap: (){
-                            onLetterTap("B");},
-                          child: Container(
-                            width: width,
-                            height: height,
-                            padding: padding,
-                            decoration: BoxDecoration(borderRadius: BorderRadius.circular(12),
-                            color: Colors.green,),
-                            child: Text("B 🎶", style: TextStyle(fontSize: 30, color: Colors.white),),),
-                        ),
+                    buildNoteContainer(letter: "B", color: Colors.green, width: 280),
                     // 3rd row
                     SizedBox(height: sizedBox,),
-                    GestureDetector(
-                      onTap: (){
-                        onLetterTap("C");},
-                      child: Container(
-                        width: width,
-                        height: height,
-                        padding: padding,
-                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12),
-                        color: Colors.blue,),
-                        child: Text("C  🎶", style: TextStyle(fontSize: 30, color: Colors.white),),),
-                    ),
+                    buildNoteContainer(letter: "C", color: Colors.blue, width: 250),
                     // fourth row
                     SizedBox(height: sizedBox,),
-                        GestureDetector(
-                          onTap: (){
-                            onLetterTap("D");
-                          },
-                          child: Container(
-                            width: width,
-                            height: height,
-                            padding: padding,
-                            decoration: BoxDecoration(borderRadius: BorderRadius.circular(12),
-                            color: Colors.yellowAccent,),
-                            child: Text("D  🎶", style: TextStyle(fontSize: 30, color: Colors.white),),
-                          ),
-                        ),
+                    buildNoteContainer(letter: "D", color: Colors.yellow, width: 220),
                         // 5th row
                     SizedBox(height: sizedBox,),
-                    GestureDetector(
-                      onTap: (){
-                        onLetterTap("E");
-                      },
-                      child: Container(
-                        width: width,
-                        height: height,
-                        padding: padding,
-                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12),
-                        color: Colors.deepOrange,),
-                        child: Text("E 🎶", style: TextStyle(fontSize: 30, color: Colors.white),),
-                      ),
-                    ),
+                    buildNoteContainer(letter: "E", color: Colors.orange, width: 190),
                         // 6th row
                     SizedBox(height: sizedBox,),
-                    GestureDetector(
-                      onTap: (){
-                        onLetterTap("F");
-                      },
-                      child: Container(
-                        width: width,
-                        height: height,
-                        padding: padding,
-                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12),
-                        color: Colors.purple,),
-                        child: Text("F 🎶", style: TextStyle(fontSize: 30, color: Colors.white),),
-                      ),
-                    ),
+                    buildNoteContainer(letter: "F", color: Colors.purple, width: 160),
                         // 7th row
                     SizedBox(height: sizedBox,),
-                    GestureDetector(
-                      onTap: (){
-                        onLetterTap("G");
-                      },
-                      child: Container(
-                        width: width,
-                        height: height,
-                        padding: padding,
-                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12),
-                          color: Colors.deepPurple,),
-                        child: Text("G 🎶", style: TextStyle(fontSize: 30, color: Colors.white),),
-                      ),
-                    ),
+                    buildNoteContainer(letter: "G", color: Colors.pinkAccent, width: 130),
                   ],
                 ),
               ),
